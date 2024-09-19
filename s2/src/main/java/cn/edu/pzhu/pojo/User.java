11577792694fa60ca7ca6c425de4e6b8fdbf755c
lanquanxiang <lanquanxiang@gmail.com>
2024-09-19 09:02:53 +0800

    update

---
+++ s2/src/main/java/cn/edu/pzhu/pojo/User.java
@@ -0,0 +1,37 @@
+package cn.edu.pzhu.pojo;
+
+public class User {
+	private String username;
+	private String password;
+	private Integer status;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(String username, String password, Integer status) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.status = status;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+
+}
